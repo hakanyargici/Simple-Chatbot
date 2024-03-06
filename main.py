271b5e37d0681d0d87cd96f3788ea1dcf24c81b9
Hakan Yargıcı <hakanyargici@Hakan.local>
2024-03-06 13:53:00 +0300

    İkinci soru yüklendi.

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ bot_name = "Yudu"
 birth_year = 2024
 print(f"Hello! My name is {bot_name}.\nI was created in {birth_year}.")
 
+#Question Two
+your_name = input("Please, remind me your name.\n> ")
+print(f"What a great name you have, {your_name.title()}!")
